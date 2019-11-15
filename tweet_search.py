e28de394ec6b93123a44f0846d39c2ee602fd7ba
Kate Schneider <57326224+kteschneider@users.noreply.github.com>
2019-11-15 12:18:25 -0500

    Update tweet_search.py

--- tweet_search.py
+++ tweet_search.py
@@ -11,10 +11,10 @@ from tweepy import OAuthHandler
 User credentials to access twitter API
 '''
 
-access_token = 'api key'
-access_token_secret = 'secret api key'
-consumer_key = 'consumer key'
-consumer_secret = 'secret consumer key'
+access_token = '1149713584349470726-Jhg4lRPDr4KhQvTptLzln95EXJbvjp'
+access_token_secret = 'kPbL8n2wqZJ467ci8F3UJfkPGSN56qqyFfu9xFq16UPnI'
+consumer_key = 'fgLl7cExe3Wi3ufYPtqCsmtse'
+consumer_secret = 'Q0wsv6CJ12gLjq9Gw3aq6eL259HWG9dOGuPe46xM4K60WIXPgR'
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -52,7 +52,7 @@ def user_location(search_words, date_since):
     tweets = tw.Cursor(api.search,
                   q=search_words,
                   lang="en",
-                  since=date_since).items(20)
+                  since=date_since).items(5)
     
     users_locs = [[tweet.user.screen_name, tweet.user.location] for tweet in tweets]
     
@@ -71,3 +71,18 @@ Running the functions
 
 print(tweet_search(search_words, date_since))
 print(user_location(search_words, date_since))
+
+'''
+Printing the tweets to a text file tweets.txt
+'''
+
+f = open("tweets.txt",'w')
+
+print("List of tweets", file=f)
+for tweet in tweet_search(search_words, date_since):
+    print("Tweet no.", tweet_search(search_words, date_since).index(tweet)+1, file=f)
+    print(tweet, file=f)
+
+print("List of users and locations", file=f)
+for user in user_location(search_words, date_since):
+    print(user, file=f)
